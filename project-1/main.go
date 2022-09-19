fb45611a6b548f8d62f51f785ef9979a1896289c
ZzAlexDev <zzalexdev@gmail.com>
2022-09-19 14:52:04 +0700

    +

--- project-1/main.go
+++ project-1/main.go
@@ -3,8 +3,32 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
+/*
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+
+}
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
+
+func main() {
+	fmt.Println(math.Pi)
+}
+*/
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	x := rand.Intn(100)
+	y := rand.Intn(100)
+	fmt.Println("My number is", x)
+	fmt.Println("My number is", y)
+	fmt.Println(add(x, y))
 }
